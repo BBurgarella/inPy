ec3f25f3fce8f96def3d01fefcbd297f1e0ecd0a
Boris Burgarella <boris.burgarella@polymtl.ca>
2019-02-21 13:26:22 -0500

    Update

---
+++ AbaqusScripting/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from abaqus import *
+from abaqusConstants import *
+import regionToolset
+from part import *
+from material import *
+from section import *
+from assembly import *
+from step import *
+from interaction import *
+from load import *
+from mesh import *
+from optimization import *
+from job import *
+from sketch import *
+from visualization import *
+from connectorBehavior import *
+import sketch
+import part
+
+from inPy_Abq_Classes import *
+from inPy_Abq_Functions import *
